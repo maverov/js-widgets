efa1cad2a297b69b7f8fea8a657df65cb6f0b84b
Hristiyan Maverov <maverov@gmail.com>
2021-02-10 05:25:14 +0200

    New widgets added.

---
+++ Double_Vertical_Slider/script.js
@@ -0,0 +1,12 @@
+const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const slideLeft = document.querySelector(".left-slide");
+const upBtn = document.querySelector(".up-button");
+const downBtn = document.querySelector(".down-button");
+const slidesLength = slideRight.querySelectorAll("div").length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upButton.addEventListener("click")
---
+++ Toast_Notification/script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("button");
+const toasts = document.getElementById("toasts");
+
+const messages = [
+    "Message One",
+    "Message 2",
+    "Yo!",
+    "Click Me",
+]
+
+button.addEventListener("click", createNotification);
+
+function createNotification() {
+    const toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.innerText = getRandomMessage();
+
+    toasts.appendChild(toast)
+
+    setTimeout(() => {
+        toast.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
